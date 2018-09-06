4053773ff3b4930b22810390231a8e4f8e7d6c44
derteta <derteta@gmx.de>
2018-09-06 22:31:07 +0200

    Refactor: Extract common base class for Connectors
    to unify the handling of a connection

--- ftplugin/python/connectors.py
+++ ftplugin/python/connectors.py
@@ -1,53 +1,51 @@
 from connection import Connection
 
 
-class ClientConnector(object):
+class ConnectionHolder(object):
 
-  def __init__(self, socket_factory):
-    super(ClientConnector, self).__init__()
-    self._connection = None
-    self._server_socket = socket_factory()
-    if self._server_socket:
-      self._check_for_new_connection_to_client()
+  _connection = None
 
-  def _check_for_new_connection_to_client(self):
-    connection_socket, _ = self._server_socket.accept() \
-      if self._server_socket \
-      else (None, '')
-    if connection_socket:
-      self._connection = Connection(connection_socket)
+  def _setup_connection(self, socket):
+    if socket:
+      self._connection = Connection(socket)
+
+  @property
+  def connection(self):
+    return self._connection
 
   def disconnect(self):
     if self._connection:
       self._connection.close()
       self._connection = None
 
+
+class ClientConnector(ConnectionHolder):
+
+  def __init__(self, socket_factory):
+    super(ClientConnector, self).__init__()
+    self._server_socket = socket_factory()
+    self._check_for_new_connection_to_client()
+
+  def _check_for_new_connection_to_client(self):
+    connection_socket, _ = self._server_socket.accept() \
+      if self._server_socket \
+      else (None, '')
+    self._setup_connection(connection_socket)
+
+  def disconnect(self):
+    super(ClientConnector, self).disconnect()
+
     if self._server_socket:
       self._server_socket.close()
       self._server_socket = None
 
-  @property
-  def connection(self):
-    return self._connection
 
-
-class ServerConnector(object):
+class ServerConnector(ConnectionHolder):
 
   def __init__(self, socket_factory):
     super(ServerConnector, self).__init__()
-    self._connection = None
     self._check_for_connection_to_server(socket_factory)
 
   def _check_for_connection_to_server(self, socket_factory):
     connection_socket = socket_factory()
-    if connection_socket:
-      self._connection = Connection(connection_socket)
-
-  def disconnect(self):
-    if self._connection is not None:
-      self._connection.close()
-      self._connection = None
-
-  @property
-  def connection(self):
-    return self._connection
+    self._setup_connection(connection_socket)
