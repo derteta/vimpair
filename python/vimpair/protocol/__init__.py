e79c8b2f8191d3da638996f2eb84cd5ec56b2f16
derteta <derteta@gmx.de>
2019-02-24 12:33:24 +0100

    Move the protocol module into its own subfolder
    This is a precursor before splitting up the module

---
+++ python/vimpair/protocol/__init__.py
@@ -0,0 +1,19 @@
+from .protocol import (
+    FULL_UPDATE_PREFIX,
+    UPDATE_START_PREFIX,
+    UPDATE_PART_PREFIX,
+    UPDATE_END_PREFIX,
+    CURSOR_POSITION_PREFIX,
+    TAKE_CONTROL_MESSAGE,
+    FILE_CHANGE_PREFIX,
+    SAVE_FILE_MESSAGE,
+    MESSAGE_LENGTH,
+
+    generate_contents_update_messages,
+    generate_cursor_position_message,
+    generate_file_change_message,
+    generate_save_file_message,
+    generate_take_control_message,
+
+    MessageHandler,
+)
